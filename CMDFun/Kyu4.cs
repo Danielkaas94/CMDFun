ff888f157e69ef1ee6e7bbc7bce21eae242d2e6f
Danielkaas94 <danielkj94@hotmail.com>
2019-06-02 17:23:15 +0200

    New Kyu4 class

---
+++ CMDFun/Kyu4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMDFun
+{
+    class Kyu4
+    {
+        // https://www.codewars.com/kata/next-bigger-number-with-the-same-digits/train/csharp
+        /// <summary>
+        /// 12 ==> 21
+        /// 513 ==> 531
+        /// 2017 ==> 2071
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long NextBiggerNumber(long n)
+        {
+            //code me
+            return 0;
+        }
+
+    }
+}
