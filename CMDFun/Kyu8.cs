725bec2cf87850c0ae5a74f021509701a75539a0
Daniel Kaas <danielkj94@hotmail.com>
2021-03-03 10:19:54 +0100

    Paperwork - Kyu8.cs 📜
    Paperwork + Additional solutions 🤯

--- CMDFun/Kyu8.cs
+++ CMDFun/Kyu8.cs
@@ -9,6 +9,33 @@ namespace CMDFun
     class Kyu8
     {
         
+        // https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/csharp
+        /// <summary>
+        /// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+        /// Your task is to calculate how many blank pages do you need.
+        /// </summary>
+        /// <param name="n">Number of classmates</param>
+        /// <param name="m">Number of pages</param>
+        /// <returns></returns>
+        public static int Paperwork(int n, int m)
+        {
+            if (n < 0 || m < 0)
+            {
+                return 0;
+            }
+
+            return n * m;
+        }
+
+        public static int Paperwork2(int n, int m) => (n > 0 && m > 0) ? m * n : 0;
+
+        public static int Paperwork3(int n, int m)
+        {
+            return (n < 0 | m < 0) ? 0 : n * m;
+        }
+
+        public static int Paperwork4(int n, int m) => Math.Max(n, 0) * Math.Max(m, 0);
+        
         // https://www.codewars.com/kata/55685cd7ad70877c23000102/csharp
         /// <summary>
         /// given a number and have to make it negative
