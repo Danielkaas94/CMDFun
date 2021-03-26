b857d0dc1a8b19ea4a536cc526c0d585b2612183
Daniel Kaas <danielkj94@hotmail.com>
2021-03-26 12:05:02 +0100

    Spring Cleaning - Program.cs ✨🧹
    Some Spring Easter Cleaning is great 🐰🐇🐤🐣🐥✨🧹
    
        • Remove string s
        • Console.WriteLine(Kyu6.Catalog("saw", s));
        • Console.WriteLine(Kyu8.OddCount()
        • Console.WriteLine(Kyu6.IsPrime());

--- CMDFun/Program.cs
+++ CMDFun/Program.cs
@@ -15,57 +15,14 @@ namespace CMDFun
         static void Main(string[] args)
         {
             
-            Kyu7.AdjacentElementsProduct(new int[] { 1, 2, 3 });                                // 6
-            Kyu7.AdjacentElementsProduct(new int[] { 9, 5, 10, 2, 24, -1, -48 });               // 50
+            Kyu7.AdjacentElementsProduct(new int[] { 1, 2, 3 });                                //  6
+            Kyu7.AdjacentElementsProduct(new int[] { 9, 5, 10, 2, 24, -1, -48 });               //  50
             Kyu7.AdjacentElementsProduct(new int[] { -23, 4, -5, 99, -27, 329, -2, 7, -921 });  // -14
-            Kyu7.AdjacentElementsProduct(new int[] { 0, 1, 0, 1, 0, 1000 });                    // 0
-            
+            Kyu7.AdjacentElementsProduct(new int[] { 0, 1, 0, 1, 0, 1000 });                    //  0
             
             Kyu7.Spoonerize("Reanu Keeves");
             Kyu7.Spoonerize("Nitch Bigger");
             Kyu7.Spoonerize("Pig Benis");
-
-            Console.ReadLine();
-            
-            
-            string s =
-    "<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>\n\n" +
-    "<prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>\n\n" +
-    "<prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>\n\n" +
-    "<prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>\n\n" +
-    "<prod><name>saw</name><prx>9</prx><qty>10</qty></prod>\n\n" +
-    "<prod><name>chair</name><prx>100</prx><qty>20</qty></prod>\n\n" +
-    "<prod><name>fan</name><prx>50</prx><qty>8</qty></prod>\n\n" +
-    "<prod><name>wire</name><prx>10.8</prx><qty>15</qty></prod>\n\n" +
-    "<prod><name>battery</name><prx>150</prx><qty>12</qty></prod>\n\n" +
-    "<prod><name>pallet</name><prx>10</prx><qty>50</qty></prod>\n\n" +
-    "<prod><name>wheel</name><prx>8.80</prx><qty>32</qty></prod>\n\n" +
-    "<prod><name>extractor</name><prx>105</prx><qty>17</qty></prod>\n\n" +
-    "<prod><name>bumper</name><prx>150</prx><qty>3</qty></prod>\n\n" +
-    "<prod><name>ladder</name><prx>112</prx><qty>12</qty></prod>\n\n" +
-    "<prod><name>hoist</name><prx>13.80</prx><qty>32</qty></prod>\n\n" +
-    "<prod><name>platform</name><prx>65</prx><qty>21</qty></prod>\n\n" +
-    "<prod><name>car wheel</name><prx>505</prx><qty>7</qty></prod>\n\n" +
-    "<prod><name>bicycle wheel</name><prx>150</prx><qty>11</qty></prod>\n\n" +
-    "<prod><name>big hammer</name><prx>18</prx><qty>12</qty></prod>\n\n" +
-    "<prod><name>saw for metal</name><prx>13.80</prx><qty>32</qty></prod>\n\n" +
-    "<prod><name>wood pallet</name><prx>65</prx><qty>21</qty></prod>\n\n" +
-    "<prod><name>circular fan</name><prx>80</prx><qty>8</qty></prod>\n\n" +
-    "<prod><name>exhaust fan</name><prx>62</prx><qty>8</qty></prod>\n\n" +
-    "<prod><name>cattle prod</name><prx>990</prx><qty>2</qty></prod>\n\n" +
-    "<prod><name>window fan</name><prx>62</prx><qty>8</qty></prod>";
-
-            //Console.WriteLine(Kyu6.Catalog("saw", s));
-
-            Console.WriteLine(Kyu8.OddCount(1));
-            Console.WriteLine(Kyu8.OddCount(3));
-            Console.WriteLine(Kyu8.OddCount(7));
-            Console.WriteLine(Kyu8.OddCount(15023));
-            Console.ReadLine();
-
-            Console.WriteLine(Kyu6.IsPrime(7919)); // Expect True
-            Console.WriteLine(Kyu6.IsPrime(7920)); // Expect False 
-            Console.ReadLine();
 
             PrintDiamond();
         }
